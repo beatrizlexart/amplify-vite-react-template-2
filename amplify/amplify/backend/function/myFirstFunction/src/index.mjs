e350a9f1b1e96d2f867ebcb622928a99b9f86cb1
beatrizlexart <beatriz.rosa@lexartlabs.xyz>
2025-01-08 14:46:26 -0300

    change for mjs

---
+++ amplify/amplify/backend/function/myFirstFunction/src/index.mjs
@@ -0,0 +1,15 @@
+import { createServer, proxy } from "aws-serverless-express";
+import app from "./app"; // O app com a lógica da Lambda
+
+/**
+ * @type {import('http').Server}
+ */
+const server = createServer(app);
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+export function handler(event, context) {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  return proxy(server, event, context, "PROMISE").promise;
+}
